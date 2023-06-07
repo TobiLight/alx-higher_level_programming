ff0876f72c46af5a8a6c0cd2b5337930070c8322
TobiLight <jgetitdonefast@gmail.com>
2023-06-06 17:49:02 -0700

    Program that prints numbers from 0 to 99 separated by a command and space

--- 0x01-python-if_else_loops_functions/5-print_comb2.py
+++ 0x01-python-if_else_loops_functions/5-print_comb2.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 for i in range(100):
     if i < 10:
-        print("0{0}".format(i), end=", ")
+        print("{:02d}".format(i), end=", ")
         continue
     if i == 99:
         print("{}".format(i))
