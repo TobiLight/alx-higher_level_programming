177f31492fbc16108b541c80583f899e905fea50
TobiLight <jgetitdonefast@gmail.com>
2023-06-06 18:08:00 -0700

    Program that prints numbers from 0 to 99 separated by a comma and spac

--- 0x01-python-if_else_loops_functions/5-print_comb2.py
+++ 0x01-python-if_else_loops_functions/5-print_comb2.py
@@ -1,9 +1,6 @@
 #!/usr/bin/python3
 for i in range(100):
-    if i < 10:
+    if i != 99:
         print("{:02d}".format(i), end=", ")
-        continue
-    if i == 99:
-        print("{}".format(i))
-        break
-    print("{}".format(i), end=", ")
+    else:
+        print(i)
