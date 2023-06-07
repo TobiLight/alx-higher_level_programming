74e0070a3bd1e76437701732e8cea9367a488311
TobiLight <jgetitdonefast@gmail.com>
2023-06-06 19:10:47 -0700

    Function that prints a string in uppercase followed by a new line.

--- 0x01-python-if_else_loops_functions/8-uppercase.py
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -1,5 +1,10 @@
 #!/usr/bin/python3
-def uppercase(c):
-    if ord(c) >= 65 and ord(c) <= 90:
-        return True
-    return False
+def uppercase(str):
+    uppercase_string = ""
+    for char in str:
+        if ord(char) >= 97 and ord(char) <= 122:
+            uppercase_char = chr(ord(char) - 32)
+        else:
+            uppercase_char = char
+        uppercase_string += uppercase_char
+    return uppercase_string
