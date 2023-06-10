888665a72ea9de63dd753ac9b427939565648a66
TobiLight <jgetitdonefast@gmail.com>
2023-06-10 15:04:50 -0700

    Program that prints all the names defined by the compiled module

--- 0x02-python-import_modules/4-hidden_discovery.py
+++ 0x02-python-import_modules/4-hidden_discovery.py
@@ -1,10 +1,12 @@
 #!/usr/bin/python3
 # File: 4-hidden_discovery.py
 # Author: Oluwatobiloba Light
-import hidden_4
 
-names = dir(hidden_4)
-
-for i in range(len(names)):
-    if "__" not in names[i]:
-        print("{}".format(names[i]))
+if __name__ == "__main__":
+    """Print all the names defined by the compiled
+       module hidden_4.pyc"""
+    import hidden_4
+    names = dir(hidden_4)
+    for i in range(len(names)):
+        if "__" not in names[i]:
+            print("{}".format(names[i]))
