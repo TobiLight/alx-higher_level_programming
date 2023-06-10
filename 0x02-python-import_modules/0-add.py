7da634fa02ae1f21ad40cc0c87b994159441d614
TobiLight <jgetitdonefast@gmail.com>
2023-06-10 10:45:15 -0700

    Program that imports a function

--- 0x02-python-import_modules/0-add.py
+++ 0x02-python-import_modules/0-add.py
@@ -1,5 +1,10 @@
 #!/usr/bin/python3
-a = 1
-b = 2
-from add_0 import add
-print("{} + {} = {}".format(a, b, add(a, b)))
+# File: 0-add.py
+# Author: Oluwatobiloba Light
+if __name__ == "__main__":
+    """ Prints the sum of 1 & 2"""
+    from add_0 import add
+
+    a = 1
+    b = 2
+    print("{} + {} = {}".format(a, b, add(a, b)))
