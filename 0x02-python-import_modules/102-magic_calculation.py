def779125d97a174f1362dd766ef6fe03fe07fcc
TobiLight <jgetitdonefast@gmail.com>
2023-06-11 09:33:48 -0700

    Translate Python Bytecode to Python code

--- 0x02-python-import_modules/102-magic_calculation.py
+++ 0x02-python-import_modules/102-magic_calculation.py
@@ -2,11 +2,10 @@
 # File: 102-magic_calculation.py
 # Author: Oluwatobiloba Light
 
-
 def magic_calculation(a, b):
     """Convert Bytecode to Python code"""
     from magic_calculation_102 import add, sub
-    
+
     if a < b:
         c = add(a, b)
         for i in range(4, 6):
