17ce9aeb1ee0c11dc0d69520fbe1f96e2b22af86
TobiLight <jgetitdonefast@gmail.com>
2023-06-12 13:24:44 +0100

    Function that adds 2 tuples.

--- 0x03-python-data_structures/7-add_tuple.py
+++ 0x03-python-data_structures/7-add_tuple.py
@@ -18,17 +18,14 @@ def add_tuple(tuple_a=(), tuple_b=()):
         of each tuple, and the second element is the addition
         of the second element of each tuple.
     """
-    if len(tuple_a) == 0 and len(tuple_b) == 0:
-        return ()
-    elif len(tuple_a) == 1 and len(tuple_b) == 1:
-        return (tuple_a[0] + tuple_b[0], 0)
-    elif len(tuple_a) < 1 and len(tuple_b) > 0:
-        return tuple_b
-    elif len(tuple_a) > 0 and len(tuple_b) < 1:
-        return tuple_a
-    elif len(tuple_a) > 1 and len(tuple_b) == 1:
-        return (tuple_a[0] + tuple_b[0], tuple_a[1])
-    elif len(tuple_a) == 1 and len(tuple_b) > 1:
-        return (tuple_a[0] + tuple_b[0], tuple_b[1])
-    else:
-        return (tuple_a[0] + tuple_b[0], tuple_a[1] + tuple_b[1])
+    # Extract elements from tuple_a
+    a1 = tuple_a[0] if len(tuple_a) > 0 else 0
+    a2 = tuple_a[1] if len(tuple_a) > 1 else 0
+
+    # Extract elements from tuple_b
+    b1 = tuple_b[0] if len(tuple_b) > 0 else 0
+    b2 = tuple_b[1] if len(tuple_b) > 1 else 0
+
+    # Perform addition and return the result as a new tuple
+    result = (a1 + b1, a2 + b2)
+    return result
