515a3cb65c480609c16205cf068c2d35d0259d3e
TobiLight <jgetitdonefast@gmail.com>
2023-06-13 06:16:56 +0100

    Modified code

--- 0x03-python-data_structures/13-is_palindrome.c
+++ 0x03-python-data_structures/13-is_palindrome.c
@@ -7,6 +7,15 @@
 #include "stdlib.h"
 
 listint_t *reverse_list(listint_t **head);
+void popint(int *arr, int *size);
+
+/**
+ * popint - Pops an item from an array
+ * @arr: The array of items
+ * @size: The size of the array
+ *
+ * Returns: Nothing
+ */
 
 /**
  * reverse_list - Reverses a singly linked listint_t
@@ -17,18 +26,18 @@ listint_t *reverse_list(listint_t **head);
  */
 listint_t *reverse_list(listint_t **head)
 {
-    listint_t *current = *head, *next, *prev = NULL;
+	listint_t *current = *head, *next, *prev = NULL;
 
-    while (current)
-    {
-        next = current->next;
-        current->next = prev;
-        prev = current;
-        current = next;
-    }
+	while (current)
+	{
+		next = current->next;
+		current->next = prev;
+		prev = current;
+		current = next;
+	}
 
-    *head = prev;
-    return (*head);
+	*head = prev;
+	return (*head);
 }
 
 /**
@@ -40,34 +49,34 @@ listint_t *reverse_list(listint_t **head)
 
 int is_palindrome(listint_t **head)
 {
-    listint_t *temp, *reverse, *middle;
-    size_t size = 0, i;
+	listint_t *temp, *reverse, *middle;
+	size_t size = 0, i;
 
-    if (*head == NULL || (*head)->next == NULL)
-        return (1);
+	if (*head == NULL || (*head)->next == NULL)
+		return (1);
 
-    temp = *head;
-    while (temp)
-    {
-        size++;
-        temp = temp->next;
-    }
-    temp = *head;
-    for (i = 0; i < (size / 2) - 1; i++)
-        temp = temp->next;
-    if ((size % 2) == 0 && temp->n != temp->next->n)
-        return (0);
-    temp = temp->next->next;
-    reverse = reverse_list(&temp);
-    middle = reverse;
-    temp = *head;
-    while (reverse)
-    {
-        if (temp->n != reverse->n)
-            return (0);
-        temp = temp->next;
-        reverse = reverse->next;
-    }
-    reverse_list(&middle);
-    return (1);
+	temp = *head;
+	while (temp)
+	{
+		size++;
+		temp = temp->next;
+	}
+	temp = *head;
+	for (i = 0; i < (size / 2) - 1; i++)
+		temp = temp->next;
+	if ((size % 2) == 0 && temp->n != temp->next->n)
+		return (0);
+	temp = temp->next->next;
+	reverse = reverse_list(&temp);
+	middle = reverse;
+	temp = *head;
+	while (reverse)
+	{
+		if (temp->n != reverse->n)
+			return (0);
+		temp = temp->next;
+		reverse = reverse->next;
+	}
+	reverse_list(&middle);
+	return (1);
 }
