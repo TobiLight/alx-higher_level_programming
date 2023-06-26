01df50c039b9830e9c5db3425bd978a684c80250
TobiLight <jgetitdonefast@gmail.com>
2023-06-27 00:51:49 +0100

    Function that raises a type exception

---
+++ 0x05-python-exceptions/5-raise_exception.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+# File: 5-raise_exception.py
+# Author: Oluwatobiloba Light
+
+
+def raise_exception():
+    """
+    Raises a type exception.add()
+
+    Raises:
+        TypeError
+    """
+    raise TypeError
