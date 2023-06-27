1ddb46777c168f31c00039038e54289965e5a282
TobiLight <jgetitdonefast@gmail.com>
2023-06-28 00:35:08 +0100

    Class square method that prints square with '#' characters

--- 0x06-python-classes/5-square.py
+++ 0x06-python-classes/5-square.py
@@ -78,6 +78,7 @@ class Square:
         """
         if self.__size == 0:
             print("")
+            return
         for i in range(self.__size):
             for j in range(self.__size):
                 print("#", end="")
