429e082e597b673c4bada20fa3ddcd6fe365395e
TobiLight <jgetitdonefast@gmail.com>
2023-06-28 00:10:20 +0100

    Class Square with size instantiation and validation

---
+++ 0x06-python-classes/2-square.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+# File: 0-square.py
+# Author: Oluwatobiloba Light
+"""Define a class Square."""
+
+
+class Square:
+    """
+    Represents a square.
+    """
+
+    def __init__(self, size=0):
+        if not isinstance(size, int):
+            raise TypeError('size must be an integer')
+        if size < 0:
+            raise ValueError('size must be >= 0')
+        else:
+            self.__size = size
