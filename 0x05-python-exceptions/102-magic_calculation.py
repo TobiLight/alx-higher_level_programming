402fe3b51af1392c64bd9ac49a590358d48282cb
TobiLight <jgetitdonefast@gmail.com>
2023-06-27 03:16:26 +0100

    Function that does magic calculation

---
+++ 0x05-python-exceptions/102-magic_calculation.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python3
+# File: 100-safe_print_integer_err.py
+# Author: Oluwatobiloba Light
+
+
+def magic_calculation(a, b):
+    """
+    Function that does magic calculation like bytecode
+
+    Args:
+        a (int): Integer
+        b (int): Integer
+
+    Returns:
+        - Integer
+    """
+    result = 0
+    for i in range(1, 3):
+        try:
+            if i > a:
+                raise Exception("Too far")
+            else:
+                result += (a + b) ** i / i
+        except Exception as e:
+            result = result + a + b
+            break
+    return result
