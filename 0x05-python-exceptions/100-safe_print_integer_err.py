270a08b22699daa9b7fab530a4b6a447d7cf4a44
TobiLight <jgetitdonefast@gmail.com>
2023-06-27 01:02:15 +0100

    Function that prints an integer

---
+++ 0x05-python-exceptions/100-safe_print_integer_err.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python3
+# File: 100-safe_print_integer_err.py
+# Author: Oluwatobiloba Light
+
+
+def safe_print_integer_err(value):
+    """
+    Prints an integer
+
+    Args:
+        value (any): can be any type (integer, string, etc.)
+
+    Returns:
+        - True if value has been correctly printed (it means the value is an integer)
+        - Otherwise False and prints in stderr the error precede by Exception:
+    """
+    is_integer = False
+    try:
+        if isinstance(abs(value), int):
+            print("{:d}".format(value))
+            is_integer = True
+        else:
+            is_integer = False
+    except:
+        pass
+    return is_integer
