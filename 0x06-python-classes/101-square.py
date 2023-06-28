3c1b671965641a5cc654974caa382c280ff735a5
TobiLight <jgetitdonefast@gmail.com>
2023-06-28 03:12:42 +0100

    Modified code

--- 0x06-python-classes/101-square.py
+++ 0x06-python-classes/101-square.py
@@ -32,8 +32,8 @@ class Square:
             size (int): The size of the square (default is 0).
             position (tuple): The position of the square (default is (0, 0))
         """
-        self.size = size
-        self.position = position
+        self.__size = size
+        self.__position = position
 
     @property
     def size(self):
