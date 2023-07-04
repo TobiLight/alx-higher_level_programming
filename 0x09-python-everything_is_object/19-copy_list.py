60213eec07be96847274fe2ace84487842a2b48f
TobiLight <jgetitdonefast@gmail.com>
2023-07-04 23:54:28 +0100

    Modified code

--- 0x09-python-everything_is_object/19-copy_list.py
+++ 0x09-python-everything_is_object/19-copy_list.py
@@ -8,11 +8,12 @@ def copy_list(l):
     """
     Return a copy of a list.
 
-    Arguments:
-        l -- The input list to be copied.
+    Args:
+        l (list): The input list to be copied.
 
     Returns:
         A new list with the same elements as the original list.
 
     """
-    return l[:]
+    new_list = l[:]
+    return new_list
