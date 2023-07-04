b4617746ac204b97ebd372f045290af2d3543407
TobiLight <jgetitdonefast@gmail.com>
2023-07-04 23:52:03 +0100

    Function that returns a copy of a list.

---
+++ 0x09-python-everything_is_object/19-copy_list.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+# File: 19-copy_list.py
+# #Author: Oluwatobiloba Light
+"""Returns copy of a list"""
+
+
+def copy_list(l):
+    """
+    Return a copy of a list.
+
+    Arguments:
+        l -- The input list to be copied.
+
+    Returns:
+        A new list with the same elements as the original list.
+
+    """
+    return l[:]
