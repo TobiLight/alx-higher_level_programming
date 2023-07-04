5ea76037426ee5b4df7e2ffe503766a9e18ca540
TobiLight <jgetitdonefast@gmail.com>
2023-07-04 23:58:16 +0100

    Modified answer

--- 0x09-python-everything_is_object/19-copy_list.py
+++ 0x09-python-everything_is_object/19-copy_list.py
@@ -1,18 +1,3 @@
 #!/usr/bin/python3
-# File: 19-copy_list.py
-# Author: Oluwatobiloba Light
-"""Returns copy of a list"""
-
-
 def copy_list(l):
-    """
-    Return a copy of a list.
-
-    Args:
-        l (list): The input list to be copied.
-
-    Returns:
-        A new list with the same elements as the original list.
-
-    """
-    return l.copy()
+    return l[:]
