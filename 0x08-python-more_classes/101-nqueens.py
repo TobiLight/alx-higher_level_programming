c07fde15d1485025ef1d25402b55d56c2422618e
TobiLight <jgetitdonefast@gmail.com>
2023-07-04 02:04:35 +0100

    Program that solves the N queens problem.

--- 0x08-python-more_classes/101-nqueens.py
+++ 0x08-python-more_classes/101-nqueens.py
@@ -16,7 +16,8 @@ Attributes:
     board (list): A list of lists representing the chessboard.
     solutions (list): A list of lists containing solutions.
 
-Solutions are represented in the format [[r, c], [r, c], [r, c], [r, c]]
+Solutions are represented and printed in the format [[r, c], [r, c], [r, c], 
+[r, c]]
 where `r` and `c` represent the row and column, respectively, where a
 queen must be placed on the chessboard.
 """
