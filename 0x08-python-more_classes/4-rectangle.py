5aa6addbf3d9501a2b08b3babacb028e07428470
TobiLight <jgetitdonefast@gmail.com>
2023-07-04 09:10:02 +0100

    Fixed pycodestyle error

--- 0x08-python-more_classes/4-rectangle.py
+++ 0x08-python-more_classes/4-rectangle.py
@@ -20,7 +20,8 @@ class Rectangle:
         area(): Returns the area of a rectangle.
         perimeter(): Returns the perimeter of a rectangle.
         __str__(): Returns the string representatio of a rectangle.
-        __repr__(): Returns a string representation of the rectangle for debugging.
+        __repr__(): Returns a string representation of the rectangle for
+                    debugging.
 
     Raises:
         TypeError: If the width or height is not an integer.
