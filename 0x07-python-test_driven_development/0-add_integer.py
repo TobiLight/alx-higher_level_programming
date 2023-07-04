b4f64a4e62032e5b8a72223815b618c17636b8db
TobiLight <jgetitdonefast@gmail.com>
2023-07-04 09:49:39 +0100

    Function that divides all elements of a matrix.

--- 0x07-python-test_driven_development/0-add_integer.py
+++ 0x07-python-test_driven_development/0-add_integer.py
@@ -9,10 +9,8 @@ TypeError If either `a` or `b` is not an integer.
 
 
 def add_integer(a, b=98):
-    """Returns the sum of a and b as an integer.
-
-    Raises:
-        TypeError: If a or b is not an integer or float.
+    """
+    Returns the sum of a and b as an integer.
     """
     if not isinstance(a, (int, float)):
         raise TypeError('a must be an integer')
