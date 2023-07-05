18b746ea39038b0aaa692405bcdc2179a6893a87
TobiLight <jgetitdonefast@gmail.com>
2023-07-05 08:10:25 +0100

    Function that prints two new lines after each character ',?:'

--- 0x07-python-test_driven_development/5-text_indentation.py
+++ 0x07-python-test_driven_development/5-text_indentation.py
@@ -18,18 +18,29 @@ def text_indentation(text):
         raise TypeError('text must be a string')
     string = ''
     i = 0
-    while i < len(text):
-        string += text[i]
-        if text[i] in ['.', '?', ':']:
-            string = string.strip()
-            print(string + '\n')
-            try:
-                if text[i + 1] == ' ':
-                    i += 1
-            except IndexError:
-                pass
-            string = ''
+    while i < len(text) and text[i] == ' ':
         i += 1
+    while i < len(text):
+        print(text[i], end="")
+        if text[i] in '.?:' or text[i] == '\n':
+            print('\n\n')
+            i += 1
+            while i < len(text) and text[i] == " ":
+                i += 1
+            continue
+        i += 1
+    # while i < len(text):
+    #     string += text[i]
+    #     if text[i] in ['.', '?', ':']:
+    #         string = string.strip()
+    #         print(string + '\n')
+    #         try:
+    #             if text[i + 1] == ' ':
+    #                 i += 1
+    #         except IndexError:
+    #             pass
+    #         string = ''
+    #     i += 1
 
-    if len(string) > 0:
-        print(string, end="")
+    # if len(string) > 0:
+    #     print(string, end="")
