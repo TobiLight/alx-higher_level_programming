b659a30c91834eb4683668845e1d9b14586db9d5
TobiLight <jgetitdonefast@gmail.com>
2023-07-11 04:40:42 +0100

    A class BaseGeometry with public instance methods.

---
+++ 0x0A-python-inheritance/6-base_geometry.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+# File: 6-base_geometry.py
+# Author: Oluwatobiloba Light
+"""Defines a base geometry class BaseGeometry."""
+
+
+class BaseGeometry:
+    """Represents a base geometry."""
+
+    def area(self):
+        """Not implemented."""
+        raise Exception("area() is not implemented")
