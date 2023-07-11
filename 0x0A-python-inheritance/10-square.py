0d8906979c101ee53c1eab953722b7ea6d869f65
TobiLight <jgetitdonefast@gmail.com>
2023-07-11 05:04:04 +0100

    a class Square that inherits from Rectangle

---
+++ 0x0A-python-inheritance/10-square.py
@@ -0,0 +1,28 @@
+#!/usr/bin/python3
+# File: 10-square.py
+# Author: Oluwatobiloba Light
+"""Defines a Rectangle subclass Square."""
+Rectangle = __import__('9-rectangle').Rectangle
+
+
+class Square(Rectangle):
+    """
+    Square class that inherits from Rectangle.
+
+    Attributes:
+        __size (int): Private size of the square.
+
+    Public Methods:
+        area(self): Calculates and returns the area of the square.
+    """
+
+    def __init__(self, size):
+        """
+        Initializes a Square instance with a given size.
+
+        Args:
+            size (int): The size of the square.
+        """
+        self.integer_validator("size", size)
+        super().__init__(size, size)
+        self.__size = size
