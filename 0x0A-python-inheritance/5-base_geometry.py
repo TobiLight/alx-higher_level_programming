f22504921d928f94093277db829bd7b15fdaa8f6
TobiLight <jgetitdonefast@gmail.com>
2023-07-11 04:35:53 +0100

    An empty class BaseGeometry.

---
+++ 0x0A-python-inheritance/5-base_geometry.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+# File: 5-base_geometry.py
+# Author: Oluwatobiloba Light
+"""Defines an empty class BaseGeometry."""
+
+
+class BaseGeometry:
+    """
+    Represent base geometry.
+    """
+    pass
