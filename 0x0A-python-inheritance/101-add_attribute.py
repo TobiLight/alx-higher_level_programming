093275fcca4a05529ca0ca0228fdad9796e793cd
TobiLight <jgetitdonefast@gmail.com>
2023-07-11 05:21:35 +0100

    Fixed pycodestyle error

--- 0x0A-python-inheritance/101-add_attribute.py
+++ 0x0A-python-inheritance/101-add_attribute.py
@@ -1,7 +1,8 @@
 def add_attribute(obj, attr_name, attr_value):
     """
-    Adds a new attribute to an object if it's possible. Raises a TypeError exception
-    with the message "can't add new attribute" if the object can't have a new attribute.
+    Adds a new attribute to an object if it's possible. Raises a 
+    TypeError exception with the message "can't add new attribute" if the 
+    object can't have a new attribute.
 
     Args:
         obj: The object to add the attribute to.
