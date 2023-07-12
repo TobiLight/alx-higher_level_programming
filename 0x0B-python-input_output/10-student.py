363ac80e91f34dab429be4fdb1ab976252b9ef48
TobiLight <jgetitdonefast@gmail.com>
2023-07-12 04:43:47 +0100

    Class Student that defines a student

---
+++ 0x0B-python-input_output/10-student.py
@@ -0,0 +1,50 @@
+#!/usr/bin/python3
+# File: 10-student.py
+# Author: Oluwatobiloba Light
+"""Defines a class Student."""
+
+
+class Student:
+    """
+    Represents a student."""
+
+    def __init__(self, first_name, last_name, age):
+        """
+        Initializes a new Student.
+
+        Args:
+            first_name (str): The first name of the student.
+            last_name (str): The last name of the student.
+            age (int): The age of the student.
+        """
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self, attrs=None):
+        """
+        Gets a dictionary representation of the Student.
+
+        If attrs is a list of strings, represents only those attributes
+        included in the list.
+
+        Args:
+            attrs (list): (Optional) The attributes to represent.
+        """
+        if (type(attrs) == list and
+                all(type(element) == str for element in attrs)):
+            return {key: getattr(self, key) for key in attrs if hasattr(self, key)}
+        return self.__dict__
+
+
+if __name__ == "__main__":
+    student_1 = Student("John", "Doe", 23)
+    student_2 = Student("Bob", "Dylan", 27)
+
+    j_student_1 = student_1.to_json()
+    j_student_2 = student_2.to_json(['first_name', 'age'])
+    j_student_3 = student_2.to_json(['middle_name', 'age'])
+
+    print(j_student_1)
+    print(j_student_2)
+    print(j_student_3)
