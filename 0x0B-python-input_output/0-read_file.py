fd67def60f2123e76e462da4a77560e7a971606c
TobiLight <jgetitdonefast@gmail.com>
2023-07-12 03:45:04 +0100

    A function that reads text from file

---
+++ 0x0B-python-input_output/0-read_file.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+# File: 0-read_file.py
+# Author: Oluwatobiloba Light
+"""Defines a function to read text from file"""
+
+
+def read_file(filename=''):
+    """
+    Reads a text from file
+
+    Args:
+        filename (str, optional): Name of the file. Defaults to ''.
+    """
+    with open(filename, encoding="utf-8") as my_file:
+        lines = my_file.read()
+        print(lines)
+
+
+if __name__ == "__main__":
+    read_file("my_file_0.txt")
