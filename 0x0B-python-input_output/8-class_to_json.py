0c11e71081c2f78c8330d8c0a7091529db283034
TobiLight <jgetitdonefast@gmail.com>
2023-07-12 13:32:56 +0100

    Function that returns the dictionary representation of a simple data structure.

--- 0x0B-python-input_output/8-class_to_json.py
+++ 0x0B-python-input_output/8-class_to_json.py
@@ -9,26 +9,3 @@ def class_to_json(obj):
     Returns the dictionary represntation of a simple data structure.
     """
     return obj.__dict__
-
-
-if __name__ == "__main__":
-    MyClass = __import__('8-my_class').MyClass
-    MyClass2 = __import__('8-my_class_2').MyClass
-
-    m = MyClass("John")
-    m.number = 89
-    print(type(m))
-    print(m)
-
-    mj = class_to_json(m)
-    print(type(mj))
-    print(mj)
-    print("==========")
-    m2 = MyClass2("John")
-    m2.win()
-    print(type(m2))
-    print(m2)
-
-    mj = class_to_json(m2)
-    print(type(mj))
-    print(mj)
