54532db39cc3cf3663e073f34b75b63ff29e9b1c
TobiLight <jgetitdonefast@gmail.com>
2023-07-12 13:35:00 +0100

    Define a class student

--- 0x0B-python-input_output/11-student.py
+++ 0x0B-python-input_output/11-student.py
@@ -33,58 +33,15 @@ class Student:
         """
         if (type(attrs) == list and
                 all(type(element) == str for element in attrs)):
-            return {k: getattr(self, k) for k in attrs
-                    if hasattr(self, k)}
+            return {key: getattr(self, key) for key in attrs
+                    if hasattr(self, key)}
         return self.__dict__
 
     def reload_from_json(self, json):
         """Replace all attributes of the Student.
 
         Args:
-            json (dict): The k/value pairs to replace attributes with.
+            json (dict): The key/value pairs to replace attributes with.
         """
-        for k, v in json.items():
-            setattr(self, k, v)
-
-
-if __name__ == "__main__":
-    import sys
-    import os
-    read_file = __import__('0-read_file').read_file
-    save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
-    load_from_json_file = __import__(
-        '6-load_from_json_file').load_from_json_file
-
-    path = sys.argv[1]
-
-    if os.path.exists(path):
-        os.remove(path)
-
-    student_1 = Student("John", "Doe", 23)
-    j_student_1 = student_1.to_json()
-    print("Initial student:")
-    print(student_1)
-    print(type(student_1))
-    print(type(j_student_1))
-    print("{} {} {}".format(student_1.first_name,
-          student_1.last_name, student_1.age))
-
-    save_to_json_file(j_student_1, path)
-    read_file(path)
-    print("\nSaved to disk")
-
-    print("Fake student:")
-    new_student_1 = Student("Fake", "Fake", 89)
-    print(new_student_1)
-    print(type(new_student_1))
-    print("{} {} {}".format(new_student_1.first_name,
-          new_student_1.last_name, new_student_1.age))
-
-    print("Load dictionary from file:")
-    new_j_student_1 = load_from_json_file(path)
-
-    new_student_1.reload_from_json(j_student_1)
-    print(new_student_1)
-    print(type(new_student_1))
-    print("{} {} {}".format(new_student_1.first_name,
-          new_student_1.last_name, new_student_1.age))
+        for key, val in json.items():
+            setattr(self, key, val)
