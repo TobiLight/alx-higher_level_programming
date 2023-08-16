dfe0e35ffea77651985751b3862907329f255b97
TobiLight <jgetitdonefast@gmail.com>
2023-08-16 02:27:24 +0100

    Prints the full description of the table first_table from the database hbtn_0c_0

--- 0x0D-SQL_introduction/5-full_table.sql
+++ 0x0D-SQL_introduction/5-full_table.sql
@@ -1,3 +1,2 @@
--- Prints the full description of the table first_table from the
--- database hbtn_0c_0
-SHOW COLUMNS;
+-- Prints the full description of the table first_table from the database hbtn_0c_0
+SHOW CREATE TABLE `first_table`;
