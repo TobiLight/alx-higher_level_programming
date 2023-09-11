54ec21fd173bcce50171bfffb162ad542ba73644
TobiLight <jgetitdonefast@gmail.com>
2023-09-11 20:55:19 +0100

    Script that prints JavaScript is amazing

--- 0x12-javascript-warm_up/0-javascript_is_amazing.js
+++ 0x12-javascript-warm_up/0-javascript_is_amazing.js
@@ -1,8 +1,3 @@
 #!/usr/bin/node
-/**
- * File: 0-javascript_is_amazing.js
- * Author: Oluwatobiloba Light
- */
-
-const myVar = 'JavaScript is amazing';
+const myVar = "JavaScript is amazing";
 console.log(myVar);
