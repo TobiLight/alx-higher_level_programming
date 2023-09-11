d46ca1021f595dd18b32de67e0c1c4260e702d6d
TobiLight <jgetitdonefast@gmail.com>
2023-09-11 22:56:23 +0100

    Script to replace the value 12 with 89

---
+++ 0x12-javascript-warm_up/12-object.js
@@ -0,0 +1,12 @@
+#!/usr/bin/node
+/**
+ * File: 12-object.js
+ * Author: Oluwatobiloba Light
+ */
+const myObject = {
+  type: "object",
+  value: 12,
+};
+console.log(myObject);
+myObject.value = 89;
+console.log(myObject);
