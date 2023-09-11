35ee8b5959bd62585474212b4775860ce58d1bdb
TobiLight <jgetitdonefast@gmail.com>
2023-09-11 22:14:36 +0100

    Script that prints the addition of 2 integers

--- 0x12-javascript-warm_up/9-add.js
+++ 0x12-javascript-warm_up/9-add.js
@@ -3,11 +3,10 @@
  * File: 9-add.js
  * Author: Oluwatobiloba Light
  */
+function add (a, b) {
+  console.log(parseInt(a) + parseInt(b));
+}
 const arg = process.argv.slice(2);
 const firstArg = arg[0];
 const secondArg = arg[1];
-
-function add(a, b) {
-	return a + b;
-}
-console.log(add(firstArg, secondArg));
+add(firstArg, secondArg);
