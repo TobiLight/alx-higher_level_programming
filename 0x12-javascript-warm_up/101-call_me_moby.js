ea9ffe3ca8b558baaf14f4f4f76417c7dde0f6ed
TobiLight <jgetitdonefast@gmail.com>
2023-09-11 23:31:09 +0100

    Function that executes x times a function.

--- 0x12-javascript-warm_up/101-call_me_moby.js
+++ 0x12-javascript-warm_up/101-call_me_moby.js
@@ -1,1 +1,14 @@
 #!/usr/bin/node
+/**
+ * File: 101-call_me_moby.js
+ * Author: Oluwatobiloba Light
+ */
+
+exports.callMeMoby = function (x, theFunction) {
+	let n = parseInt(x);
+	if (!isNaN(n)) {
+		for (let i = 0; i < n; i++) {
+			theFunction();
+		}
+	}
+}
