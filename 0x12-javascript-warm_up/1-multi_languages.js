166ed5bfb93fcf2e26e8b1702fe2ce504b7e85b4
TobiLight <jgetitdonefast@gmail.com>
2023-09-11 20:51:40 +0100

    Script that prints 3 lines

---
+++ 0x12-javascript-warm_up/1-multi_languages.js
@@ -0,0 +1,9 @@
+#!/usr/bin/node
+/**
+ * File: 1-multi_languages.js
+ * Author: Oluwatobiloba Light
+ */
+
+console.log('C is fun');
+console.log('Python is cool');
+console.log('JavaScript is amazing');
