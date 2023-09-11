676b41df9bde903bc716c75f95f3500df93d87c3
TobiLight <jgetitdonefast@gmail.com>
2023-09-11 21:49:14 +0100

    Script that prints x times “C is fun”

--- 0x12-javascript-warm_up/7-multi_c.js
+++ 0x12-javascript-warm_up/7-multi_c.js
@@ -1,2 +1,14 @@
 #!/usr/bin/node
-
+/**
+ * File: 7-multi.js
+ * Author: Oluwatobiloba Light
+ */
+const arg = process.argv.slice(2);
+if (!arg.length) {
+	console.log('Missing number of occurrences');
+}
+if (arg > 0) {
+	for (let i = 0; i < arg; i++) {
+		console.log('C is fun');
+	}
+}
