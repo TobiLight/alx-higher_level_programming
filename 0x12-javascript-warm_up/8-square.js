f88b28ee804e5f7909aacfebf1d1081fffe5fd8d
TobiLight <jgetitdonefast@gmail.com>
2023-09-11 22:07:17 +0100

    Script that prints a square

--- 0x12-javascript-warm_up/8-square.js
+++ 0x12-javascript-warm_up/8-square.js
@@ -3,17 +3,17 @@
  * File: 8-square.js
  * Author: Oluwatobiloba Light
  */
-const first_arg = process.argv.slice(2);
-const num = parseInt(first_arg[0]);
+const arg = process.argv.slice(2);
+const num = parseInt(arg[0]);
 
 if (isNaN(num)) {
-  console.log("Missing size");
+  console.log('Missing size');
 }
 if (!isNaN(num) && num > 0) {
   for (let i = 0; i < num; i++) {
-    let output = "";
+    let output = '';
     for (let j = 0; j < num; j++) {
-      output += "X";
+      output += 'X';
     }
     console.log(output);
   }
