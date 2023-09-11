8e51e915bec3259fb4e144b5f8f33dcf7c2809ef
TobiLight <jgetitdonefast@gmail.com>
2023-09-11 22:32:17 +0100

    Script that computes and prints a factorial

--- 0x12-javascript-warm_up/10-factorial.js
+++ 0x12-javascript-warm_up/10-factorial.js
@@ -4,3 +4,11 @@
  * Author: Oluwatobiloba Light
  */
 
+function factorial (num) {
+  if (isNaN(num)) return 1;
+  if (num <= 1) return 1;
+  return num * factorial(num - 1);
+}
+const arg = process.argv.slice(2);
+const num = parseInt(arg[0]);
+console.log(factorial(num));
