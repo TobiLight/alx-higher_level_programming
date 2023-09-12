cfd7fdad9338d1a5117c6e9e8bf4c0e15f0232ad
TobiLight <jgetitdonefast@gmail.com>
2023-09-12 22:17:57 +0100

    Imports an array and computes a new array.

--- 0x13-javascript_objects_scopes_closures/100-map.js
+++ 0x13-javascript_objects_scopes_closures/100-map.js
@@ -1,4 +1,11 @@
 #!/usr/bin/node
-const list = require('./100-data.js').list;
+/**
+ * File: 100-map.js
+ * Author: Oluwatobiloba Light
+ * Description: Imports an array and computes a new array.
+ */
+
+const list = require('./100-data').list;
+const newList = list.map((item, index) => item * index);
 console.log(list);
-console.log(list.map((item, index) => item * index));
+console.log(newList);
