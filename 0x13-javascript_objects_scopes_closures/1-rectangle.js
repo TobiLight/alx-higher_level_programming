a8b195767dbaa1cb9bdf3dc4e081aea41990f32e
TobiLight <jgetitdonefast@gmail.com>
2023-09-12 20:06:45 +0100

    Class Rectangle that defines a rectangle with some properties.

---
+++ 0x13-javascript_objects_scopes_closures/1-rectangle.js
@@ -0,0 +1,15 @@
+#!/usr/bin/node
+/**
+ * File: 1-rectangle.js
+ * Author: Oluwatobiloba Light
+ * Description:  Class Rectangle that defines a rectangle with some properties.
+ */
+
+class Rectangle {
+  constructor(w, h) {
+    this.width = w;
+    this.height = h;
+  }
+}
+
+module.exports = Rectangle;
