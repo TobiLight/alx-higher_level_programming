59f286abd6cba08645a90388057a2027b49c1e9c
TobiLight <jgetitdonefast@gmail.com>
2023-09-12 23:56:38 +0100

    Script that Concats 2 files

---
+++ 0x13-javascript_objects_scopes_closures/102-concat.js
@@ -0,0 +1,22 @@
+#!/usr/bin/node
+/**
+ * File: 102-concat.js
+ * Author: Oluwatobiloba Light
+ * Description: Concats 2 files.
+ */
+
+const fs = require("fs");
+const args = process.argv.slice(2);
+const fileA = args[0];
+const fileB = args[1];
+const fileC = args[2];
+
+fs.readFile(fileA, "utf-8", (err, data1) => {
+  if (err) throw err;
+  fs.readFile(fileB, "utf-8", (err, data2) => {
+    const concatData = data1 + "\n" + data2 + "\n";
+    fs.writeFile(fileC, concatData, "utf-8", (err) => {
+      if (err) throw err;
+    });
+  });
+});
