ab7c13f6c3f9f2e2d613723bab4d4f8e7686d877
TobiLight <jgetitdonefast@gmail.com>
2023-09-13 00:16:53 +0100

    Script that concats 2 files.

--- 0x13-javascript_objects_scopes_closures/102-concat.js
+++ 0x13-javascript_objects_scopes_closures/102-concat.js
@@ -5,18 +5,23 @@
  * Description: Concats 2 files.
  */
 
-const fs = require("fs");
+const fs = require('fs');
 const args = process.argv.slice(2);
-const fileA = args[0];
-const fileB = args[1];
-const fileC = args[2];
 
-fs.readFile(fileA, "utf-8", (err, data1) => {
-  if (err) throw err;
-  fs.readFile(fileB, "utf-8", (err, data2) => {
-    const concatData = data1 + "\n" + data2 + "\n";
-    fs.writeFile(fileC, concatData, "utf-8", (err) => {
-      if (err) throw err;
+function concatFiles (arg) {
+  const fileA = args[0];
+  const fileB = args[1];
+  const fileC = args[2];
+  if (!args.length) return;
+  fs.readFile(`${fileA}`, 'utf-8', (err, data1) => {
+    if (err) throw err;
+    fs.readFile(`${fileB}`, 'utf-8', (err, data2) => {
+      const concatData = data1 + '\n' + data2 + '\n';
+      fs.writeFile(`${fileC}`, concatData, 'utf-8', (err) => {
+        if (err) throw err;
+      });
     });
   });
-});
+}
+
+concatFiles(args);
