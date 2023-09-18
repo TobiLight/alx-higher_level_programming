0e1c7a896a4944227c85f25e992439f5f95e7998
TobiLight <jgetitdonefast@gmail.com>
2023-09-17 18:55:19 -0700

    Lists all states from the database hbtn_0e_0_usa

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -9,6 +9,6 @@ import MySQLdb
 
 if __name__ == "__main__":
     db = MySQLdb.connect(user=sys.argv[1], passwd=sys.argv[2], db=sys.argv[3])
-    c = db.cursor()
-    c.execute("SELECT * FROM `states`")
-    [print(state) for state in c.fetchall()]
+    conn = db.cursor()
+    conn.execute("SELECT * FROM `states`")
+    [print(state) for state in conn.fetchall()]
