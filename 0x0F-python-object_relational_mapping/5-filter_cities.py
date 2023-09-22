d52f3a042a08a5710659014f3275765b90e257a2
TobiLight <jgetitdonefast@gmail.com>
2023-09-22 23:29:24 +0100

    Takes in the name of a state as an argument and lists all cities of that state

--- 0x0F-python-object_relational_mapping/5-filter_cities.py
+++ 0x0F-python-object_relational_mapping/5-filter_cities.py
@@ -1,22 +1,27 @@
 #!/usr/bin/python3
-
-import MySQLdb as db
-from sys import argv
+# File: 5-filter_cities.py
+# Author: Oluwatobiloba Light
+"""
+Takes in the name of a state as an argument and lists all cities of that state,
+using the database hbtn_0e_4_usa
+"""
+import MySQLdb
+import sys
 
 
 if __name__ == "__main__":
     """
-    Access to the database and get the cities
+    Access to the database and get the cities of a state
     from the database.
     """
 
-    db_connect = db.connect(host="localhost", port=3306,
-                            user=argv[1], passwd=argv[2], db=argv[3])
+    db = MySQLdb.connect(user=sys.argv[1], passwd=sys.argv[2],
+                         db=sys.argv[3], port=3306)
 
-    with db_connect.cursor() as db_cursor:
-        db_cursor.execute("""
-            SELECT
-                cities.id, cities.name
+    q = db.cursor()
+    q.execute("""
+              SELECT
+                cities.name
             FROM
                 cities
             JOIN
@@ -24,13 +29,8 @@ if __name__ == "__main__":
             ON
                 cities.state_id = states.id
             WHERE
-                states.name LIKE BINARY %(state_name)s
+                states.name LIKE BINARY '{}'
             ORDER BY
                 cities.id ASC
-        """, {
-            'state_name': argv[4]
-        })
-        rows_selected = db_cursor.fetchall()
-
-    if rows_selected is not None:
-        print(", ".join([row[1] for row in rows_selected]))
+        """.format(sys.argv[4]))
+    print(", ".join([city[0] for city in q.fetchall()]))
