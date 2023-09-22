a1b53ec25768c9452b8fcc30a5f19a33655804ff
TobiLight <jgetitdonefast@gmail.com>
2023-09-22 22:59:47 +0100

    Lists all cities from the database hbtn_0e_4_usa

--- 0x0F-python-object_relational_mapping/4-cities_by_state.py
+++ 0x0F-python-object_relational_mapping/4-cities_by_state.py
@@ -1,11 +1,11 @@
 #!/usr/bin/python3
+# File: 4-cities_by_state.py
+# Author: Oluwatobiloba Light
 """
-This script lists all cities from
-the database `hbtn_0e_4_usa`.
+Lists all cities from the database `hbtn_0e_4_usa`.
 """
-
-import MySQLdb as db
-from sys import argv
+import MySQLdb
+import sys
 
 
 if __name__ == '__main__':
@@ -14,15 +14,9 @@ if __name__ == '__main__':
     from the database.
     """
 
-    db_connect = db.connect(host="localhost", port=3306,
-                            user=argv[1], passwd=argv[2], db=argv[3])
-
-    with db_connect.cursor() as db_cursor:
-        db_cursor.execute("SELECT cities.id, cities.name, states.name \
-                                FROM cities JOIN states ON cities.state_id \
-                                = states.id ORDER BY cities.id ASC")
-        rows_selected = db_cursor.fetchall()
-
-    if rows_selected is not None:
-        for row in rows_selected:
-            print(row)
+    db = MySQLdb.connect(user=sys.argv[1], passwd=sys.argv[2], db=sys.argv[3],
+                         port=3306)
+    q = db.cursor()
+    q.execute("SELECT cities.id, cities.name, states.name FROM `cities`\
+        JOIN states on cities.state_id = states.id ORDER BY 'cities.id' ASC")
+    [print(city) for city in q.fetchall()]
