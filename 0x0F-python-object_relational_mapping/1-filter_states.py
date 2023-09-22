ff9b34e5578ebc6b0d2ab0865873f7a32e49f5d0
TobiLight <jgetitdonefast@gmail.com>
2023-09-22 15:03:24 +0100

    Lists all states with a name starting with N from the database hbtn_0e_0_usa.

--- 0x0F-python-object_relational_mapping/1-filter_states.py
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -13,6 +13,6 @@ if __name__ == "__main__":
     db = MySQLdb.connect(user=sys.argv[1], passwd=sys.argv[2], db=sys.argv[3],
                          port=3306)
     q = db.cursor()
-    q.execute("SELECT * FROM `states` WHERE `name` LIKE 'N%'\
+    q.execute("SELECT * FROM `states` WHERE `name` LIKE  BINARY 'N%'\
         ORDER BY 'states.id' ASC")
     [print(state) for state in q.fetchall()]
