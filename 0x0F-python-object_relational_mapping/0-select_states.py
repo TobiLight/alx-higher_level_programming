fde6c50193b51d827779f9ddcbf8bc80723019d3
TobiLight <jgetitdonefast@gmail.com>
2023-09-22 13:26:48 +0100

    Lists all states from the database hbtn_0e_0_usa.

---
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+# File: 0-select_states.py
+# Author: Oluwatobiloba Light
+# Lists all states from the database hbtn_0e_0_usa
+# Usage: ./0-select_states.py <mysql username> \
+#                             <mysql password> \
+#                             <database name>
+
+import sys
+import MySQLdb
+
+if __name__ == "__main__":
+
+    db = MySQLdb.connect(user=sys.argv[1], passwd=sys.argv[2], db=sys.argv[3])
+    q = db.cursor()
+    q.execute("SELECT * FROM `states`")
+    [print(state) for state in q.fetchall()]
---
+++ 0x0F-python-object_relational_mapping/README.md
@@ -0,0 +1,9 @@
+<h1>0x0F. Python - Object-relational mapping</h1>
+<br />
+<br />
+
+<h2>Resources</h2>
+<small><strong>Read or watch:</strong></small>
+<ul>
+	<li><a href="https://intranet.alxswe.com/rltoken/a8DUOWhXpNX3TEwgyT-U8A">Object-relational mappers</a></li>
+</ul>
