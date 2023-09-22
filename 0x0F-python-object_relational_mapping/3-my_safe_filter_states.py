a089ad9fb91abedec0c280f68d3ba0e2d0563b11
TobiLight <jgetitdonefast@gmail.com>
2023-09-22 21:39:56 +0100

    Displays all values in the states table of hbtn_0e_0_usa where name matches the argument.

--- 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
+++ 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
@@ -5,7 +5,6 @@
 Displays all values in the states table of hbtn_0e_0_usa where name matches
 the argument
 """
-
 import MySQLdb as db
 import sys
 
@@ -15,14 +14,11 @@ if __name__ == "__main__":
     Access to the database and get the states
     from the database.
     """
-    db_connect = db.connect(host="localhost", port=3306,
-                            user=sys.argv[1], passwd=sys.argv[2],
-                            db=sys.argv[3])
+    db_connect = db.connect(user=sys.argv[1], passwd=sys.argv[2],
+                            db=sys.argv[3],  port=3306)
 
-    db_cursor = db_connect.cursor()
-    q = db_cursor.execute(
-        "SELECT * FROM states WHERE name LIKE\
-                    BINARY %(name)s ORDER BY 'states.id' ASC",
-        ({'name': sys.argv[4]},))
-    
-    [print(state) for state in q.fetchall()]
+    q = db_connect.cursor()
+    q.execute(
+        "SELECT * FROM `states` ORDER BY 'states.id' ASC")
+
+    [print(state) for state in q.fetchall() if state[1] == sys.argv[4]]
