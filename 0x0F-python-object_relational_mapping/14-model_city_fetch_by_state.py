20d8ac27649be779f064066cf94d5efff1775a32
TobiLight <jgetitdonefast@gmail.com>
2023-09-23 02:06:01 +0100

    Prints all City objects from the database hbtn_0e_14_usa

---
+++ 0x0F-python-object_relational_mapping/14-model_city_fetch_by_state.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python3
+# File: 14-model_city_fetch_by_state.py
+# Author: Oluwatobiloba Light
+"""
+Prints all City objects from the database hbtn_0e_14_usa
+"""
+import sys
+from model_state import Base, State
+from model_city import City
+from sqlalchemy import create_engine
+from sqlalchemy.orm.session import sessionmaker
+from sqlalchemy.orm import scoped_session
+
+
+if __name__ == "__main__":
+    engine = create_engine('mysql+mysqldb://{}:{}@localhost:{}/{}'
+                           .format(sys.argv[1], sys.argv[2], 3306, sys.argv[3]),
+                           pool_pre_ping=True)
+    Base.metadata.create_all(bind=engine)
+    session_factory = sessionmaker(bind=engine, expire_on_commit=False)
+    Session = scoped_session(session_factory)
+    q = Session()
+    q = q.query(City, State).order_by(City.id).filter(
+        City.state_id == State.id).all()
+    for state, city in q:
+        print("{}: ({}) {}".format(city.name, state.id, state.name))
---
+++ 0x0F-python-object_relational_mapping/model_city.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python3
+# File: model_city.py
+# Author: Oluwatobiloba Light
+"""
+Class definition of a City
+"""
+from sqlalchemy import Column, Integer, String, ForeignKey
+from sqlalchemy.ext.declarative import declarative_base
+
+Base = declarative_base()
+
+
+class City(Base):
+    """
+    City class
+
+    Attributes:
+        __tablename__ (str): The table name of the class
+        id (int): The City id of the class
+        name (str): The City name of the class
+    """
+    __tablename__ = "cities"
+    id = Column(Integer, primary_key=True, nullable=False, unique=True)
+    name = Column(String(128), nullable=False)
+    state_id = Column(Integer, ForeignKey("states.id"), nullable=False)
