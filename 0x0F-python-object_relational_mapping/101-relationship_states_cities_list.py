773282e0fd0e67dcf45d54af43411632150ca9c2
TobiLight <jgetitdonefast@gmail.com>
2023-09-23 02:28:16 +0100

    Lists all State objects, and corresponding City objects, contained in the database hbtn_0e_101_usa

--- 0x0F-python-object_relational_mapping/101-relationship_states_cities_list.py
+++ 0x0F-python-object_relational_mapping/101-relationship_states_cities_list.py
@@ -13,14 +13,13 @@ from relationship_city import City
 
 
 if __name__ == "__main__":
-    engine = create_engine('mysql+mysqldb://{}:{}@localhost:3306/{}'
+    engine = create_engine("mysql+mysqldb://{}:{}@localhost/{}"
                            .format(sys.argv[1], sys.argv[2], sys.argv[3]),
                            pool_pre_ping=True)
     Session = sessionmaker(bind=engine)
     session = Session()
 
-    q = session.query(State).order_by(State.id)
-    for item in q:
-        print("{}: {}".format(item.id, item.name))
+    for state in session.query(State).order_by(State.id):
+        print("{}: {}".format(state.id, state.name))
         for city in state.cities:
             print("    {}: {}".format(city.id, city.name))
---
+++ 0x0F-python-object_relational_mapping/102-relationship_cities_states_list.py
@@ -0,0 +1,6 @@
+#!/usr/bin/python3
+# File: 102-relationship_cities_states_list.py
+# Author: Oluwatobiloba Light
+"""
+
+"""
