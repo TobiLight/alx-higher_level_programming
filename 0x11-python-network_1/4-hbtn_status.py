4080732d0d48490d0a64bdcae5e381e358035842
TobiLight <jgetitdonefast@gmail.com>
2023-09-30 01:23:04 +0100

    Fetches https://alx-intranet.hbtn.io/status using requests

---
+++ 0x11-python-network_1/4-hbtn_status.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+# File: 4-hbtn_status.py
+# Author: Oluwatobiloba Light
+"""
+Fetches https://alx-intranet.hbtn.io/status
+"""
+import requests
+
+
+if __name__ == "__main__":
+    response = requests.get("https://alx-intranet.hbtn.io/status")
+    print("Body response:")
+    print(response)
+    print("\t- type: {}".format(type(response.text)))
+    print("\t- content: {}".format(response.text))
