77bcd0ac5a801de786db4c6e673c2c85aae40f4d
TobiLight <jgetitdonefast@gmail.com>
2023-09-30 01:33:52 +0100

    Sends a request to the URL and displays the body of the response.

--- 0x11-python-network_1/3-error_code.py
+++ 0x11-python-network_1/3-error_code.py
@@ -12,7 +12,7 @@ import sys
 
 if __name__ == "__main__":
     if len(sys.argv) > 1:
-        req = urllib.request.Request('{}'.format(sys.argv[1]))
+        req = urllib.request.Request(sys.argv[1])
         try:
             with urllib.request.urlopen(req) as response:
                 print(response.read().decode('utf-8'))
