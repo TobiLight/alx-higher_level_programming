e03116fdaec1331e29e6a85b578242a465bb216b
TobiLight <jgetitdonefast@gmail.com>
2023-09-30 02:49:17 +0100

    Sends a POST request to http://0.0.0.0:5000/search_user with the letter as a parameter

--- 0x11-python-network_1/8-json_api.py
+++ 0x11-python-network_1/8-json_api.py
@@ -12,11 +12,12 @@ import sys
 if __name__ == "__main__":
     letter = "" if len(sys.argv) == 1 else sys.argv[1]
     url = "http://0.0.0.0:5000/search_user"
-    response = requests.post(url, data={"q": letter}).json()
+    response = requests.post(url, data={"q": letter})
     try:
-        if response == {}:
+        if response.json() == {}:
             print("No result")
         else:
-            print("[{}] {}".format(response.get("id"), response.get("name")))
+            print("[{}] {}".format(response.json().get("id"),
+                                   response..json().get("name")))
     except ValueError:
         print("Not a valid JSON")
