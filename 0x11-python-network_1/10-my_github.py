b99a9d2a379a08639d2f23577f6aa03627ceafec
TobiLight <jgetitdonefast@gmail.com>
2023-09-30 03:08:22 +0100

    Uses the github API to display my ID

--- 0x11-python-network_1/10-my_github.py
+++ 0x11-python-network_1/10-my_github.py
@@ -10,6 +10,6 @@ import sys
 
 
 if __name__ == "__main__":
-    auth = HTTPBasicAuth(sys.argv[1], sys.argv[2])
+    auth = requests.auth.HTTPBasicAuth(sys.argv[1], sys.argv[2])
     response = requests.get("https://api.github.com/user", auth=auth)
     print(response.json().get("id"))
