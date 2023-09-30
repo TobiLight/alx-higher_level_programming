bc62443f2b2bd0a6336207f96a64c7eacd310465
TobiLight <jgetitdonefast@gmail.com>
2023-09-30 03:12:01 +0100

    Lists the 10 most recent commits on a given repo

---
+++ 0x11-python-network_1/100-github_commits.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+#File: 100-github_commits.py
+#Author: Oluwatobiloba Light
+"""
+Lists the 10 most recent commits on a given GitHub repository.
+"""
+import requests
+import sys
+
+
+if __name__ == "__main__":
+    url = "https://api.github.com/repos/{}/{}/commits".format(
+        sys.argv[2], sys.argv[1])
+
+    response = requests.get(url)
+    commits = response.json()
+    try:
+        for i in range(10):
+            print("{}: {}".format(
+                commits[i].get("sha"),
+                commits[i].get("commit").get("author").get("name")))
+    except IndexError:
+        pass
