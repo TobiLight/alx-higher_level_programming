5e72270edd7de2966f3b58df240ffbbf23ace0f3
TobiLight <jgetitdonefast@gmail.com>
2023-10-25 00:51:18 +0100

    Displays the status code of a GET request

---
+++ 0x14-javascript-web_scraping/2-statuscode.js
@@ -0,0 +1,12 @@
+#!/usr/bin/node
+/**
+ * File: 2-statuscode.js
+ * Author: Oluwatobiloba Light
+ * Displays the status code of a GET request.
+ */
+const req = require('request');
+
+req(process.argv[2], (err, data) => {
+  if (err) console.log(err);
+  console.log(data.statusCode);
+});
