f785ffc73c20ffa5335cb89efc550a2d145ce169
TobiLight <jgetitdonefast@gmail.com>
2023-11-02 00:15:55 +0100

    Updates the text color of the <header> element to red (#FF0000

---
+++ 0x15-javascript-web_jquery/1-script.js
@@ -0,0 +1,10 @@
+/**
+ * File: 1-script.js
+ * Author: Oluwatobiloba Light
+ * Updates the text color of the <header> element to red (#FF0000) using
+ * -- jqeury.
+ */
+
+const header = $('header');
+
+if (header) { header.css('color', '#FF0000'); }
