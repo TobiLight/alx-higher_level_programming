67786c779ec43a47a2a3595d88076b3f91e8e0ef
TobiLight <jgetitdonefast@gmail.com>
2023-11-01 23:59:06 +0100

    JavaScript script that updates the text color of the <header> element

---
+++ 0x15-javascript-web_jquery/0-script.js
@@ -0,0 +1,9 @@
+/**
+ * File: 0-script.js
+ * Author: Oluwatobiloba Light
+ * Updates the text color of the <header> element to red (#FF0000)
+ */
+
+const header = document.querySelector('header');
+
+if (header) { header.style.color = '#FF0000'; }
