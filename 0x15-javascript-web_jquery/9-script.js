f9201b599ee54108edc1b2f0efa3166290fad344
TobiLight <jgetitdonefast@gmail.com>
2023-11-02 01:24:33 +0100

    Displays the value of hello from a URL in a html tag

--- 0x15-javascript-web_jquery/9-script.js
+++ 0x15-javascript-web_jquery/9-script.js
@@ -1,12 +1,12 @@
 /**
  * File: 9-script.js
  * Author: Oluwatobiloba Light
- * Fetches from https://hellosalut.stefanbohacek.dev/?lang=fr and 
+ * Fetches from https://hellosalut.stefanbohacek.dev/?lang=fr and
  *  -- displays the value of hello from that fetch in the HTML tag DIV#hello
  */
 
-const $ = window.$
+const $ = window.$;
 
 $.get('https://hellosalut.stefanbohacek.dev/?lang=fr', res => {
-	$("div#hello").text(res.hello)
+  $('div#hello').text(res.hello);
 });
