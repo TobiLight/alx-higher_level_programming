359e04b9af85b8f34fe04adf87983833514989f8
TobiLight <jgetitdonefast@gmail.com>
2023-11-02 01:11:06 +0100

    Fetches and lists the title for all movies

---
+++ 0x15-javascript-web_jquery/8-script.js
@@ -0,0 +1,22 @@
+/**
+ * File: 8-script.js
+ * Author: Oluwatobiloba Light
+ * Fetches and lists the title for all movies by using this URL:
+ *   -- https://swapi-api.alx-tools.com/api/films/?format=json
+ */
+
+const list_movies = $('ul#list_movies');
+
+if (list_movies) {
+  $.ajax({
+    url: 'https://swapi-api.alx-tools.com/api/films/?format=json',
+    success: res => {
+      const movies = res.results;
+      for (const movie in movies) {
+        const movie_title = $(`<li>${movies[movie].title}</li>`);
+        list_movies.append(movie_title);
+      }
+      list_movies.text(res.name);
+    }
+  });
+}
