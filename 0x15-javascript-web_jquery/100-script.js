ce1916e423e1a88b138f1e40218a7a5c33ca8b92
TobiLight <jgetitdonefast@gmail.com>
2023-11-02 01:28:00 +0100

    Updates the text color of the <header> element

---
+++ 0x15-javascript-web_jquery/100-script.js
@@ -0,0 +1,10 @@
+/**
+ * File: 9-script.js
+ * Author: Oluwatobiloba Light
+ * Updates the text color of the <header> element
+ */
+
+document.addEventListener('DOMContentLoaded', () => {
+  const header = document.querySelector('header');
+  header.style.color = '#FF0000';
+});
