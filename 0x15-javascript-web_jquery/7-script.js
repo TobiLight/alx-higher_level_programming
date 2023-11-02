b5d322777dd0de576573e15cb4b786b08f517eb9
TobiLight <jgetitdonefast@gmail.com>
2023-11-02 01:01:29 +0100

    Fetches the character name from a URL

---
+++ 0x15-javascript-web_jquery/7-script.js
@@ -0,0 +1,20 @@
+/**
+ * File: 7-script.js
+ * Author: Oluwatobiloba Light
+ * Fetches the character name from this URL: 
+ * -- https://swapi-api.alx-tools.com/api/people/5/?format=json
+ */
+
+const character_name = $('div#character');
+
+if (character_name) {
+	$.ajax("https://swapi-api.alx-tools.com/api/people/5/?format=json").done((res) => {
+		character_name.text(res.name)
+	})
+	$.ajax({
+		url: "https://swapi-api.alx-tools.com/api/people/5/?format=json",
+		success: res => {
+			character_name.text(res.name)
+		}
+	})
+}
